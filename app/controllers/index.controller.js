b803846c954b24503c8d8293ce3a0949e45e9516
Poydev Developer <poydevelop@gmail.com>
2019-01-22 10:10:36 +0700

    set ENV

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -1,3 +1,4 @@
 exports.render = function(req, res){
+    //console.log(process.env.NODE_ENV);
     res.send('Hello World');
 };
--- config/express.js
+++ config/express.js
@@ -1,8 +1,22 @@
-var express = require('express');
+var express = require('express'); // http server
+var morgan = require('morgan'); //เก็บ Log
+var compression = require('compression'); //บีบอัดข้อมูล
+var bodyParser = require('body-parser'); //รับ req body
 
 
 module.exports = function(){
     var app = express();
+    if(process.env.NODE_ENV==='development'){
+        app.use(morgan('dev'));
+    } else {
+        app.use(compression);
+    }
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+
     require('../app/routes/index.routes')(app);
     return app;
 }
